f7af97745b8eb060d51332f90dc2ad5f86c7881c
1bijaysharma <bijay.sharma@creoate.com>
2026-08-13 14:51:07 +0530

    adding according

--- best-practices/src/components/Accordion.jsx
+++ best-practices/src/components/Accordion.jsx
@@ -1,5 +1,39 @@
+import { createContext, useContext, useState } from "react";
+
+const AccordionContext = createContext();
+
+const useAccordionContext = () => {
+  const context = useContext();
+  if (!context) {
+    throw new Error(
+      "Accordion-related components must be wrapped with <Accordion>",
+    );
+  }
+  return context;
+};
+
 const Accordion = ({ children, className }) => {
-  return <ul className={className}>{children}</ul>;
+  const [openItemId, setOpenItemId] = useState(null);
+
+  const openItem = (id) => {
+    setOpenItemId(id);
+  };
+
+  const closeItem = () => {
+    setOpenItemId(null);
+  };
+
+  const contextValue = {
+    openItemId,
+    openItem,
+    closeItem,
+  };
+
+  return (
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>{children}</ul>
+    </AccordionContext.Provider>
+  );
 };
 
 export default Accordion;
