dafe90d7c1a3fc12e64414db37f26d274eb5f65a
1bijaysharma <bijay.sharma@creoate.com>
2026-08-13 10:21:00 +0530

    working on accordion for compound component concept

--- best-practices/src/App.jsx
+++ best-practices/src/App.jsx
@@ -1,5 +1,40 @@
+import Accordion from "./components/Accordion";
+import AccordionItem from "./components/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h1>Why work with us?</h1>
+        <Accordion className="accordion">
+          <AccordionItem
+            className="accordion-item"
+            title="We are working with local guides"
+          >
+            <article>
+              <p>You cannot go wrong with us.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit,
+                aliquid?
+              </p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            className="accordion-item"
+            title="We are working with local guides efedfds"
+          >
+            <article>
+              <p>You cannot go wrong with us sdfsdfsd.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit,
+                aliquid?
+              </p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
