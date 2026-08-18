293f462f62c38b03aecfbcedd5881813799d1e91
1bijaysharma <bijay.sharma@creoate.com>
2026-08-18 10:16:05 +0530

    added debouncing in search

--- best-practices/src/components/SearchableList/SearchableList.jsx
+++ best-practices/src/components/SearchableList/SearchableList.jsx
@@ -1,10 +1,34 @@
-const SearchableList = ({ items }) => {
+import { useState, useRef } from "react";
+
+const SearchableList = ({ items, itemKeyFn, children }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const lastChange = useRef();
+
+  const searchResults = items.filter((item) =>
+    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
+  const handleChange = (event) => {
+    if (lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+
+    lastChange.current = setTimeout(() => {
+      lastChange.current = null;
+      setSearchTerm(event.target.value);
+    }, 500);
+  };
+
   return (
     <div className="searchable-list">
-      <input type="search" placeholder="Type your text" />
+      <input
+        type="search"
+        placeholder="Type your text"
+        onChange={handleChange}
+      />
       <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item.toString()}</li>
+        {searchResults.map((item, index) => (
+          <li key={itemKeyFn(item)}>{children(item)}</li>
         ))}
       </ul>
     </div>
